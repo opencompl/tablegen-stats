80c1957091f3d50cde5e77cc77b426c99c5a8b36
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-02-27 02:22:34 +0000

    Add tool to print IRDL test cases

---
+++ analyze-tblgen/src/analyze_tablegen/print_irdl_test_cases.py
@@ -0,0 +1,44 @@
+import argparse
+
+from analyze_tablegen.irdl import *
+from analyze_tablegen.extraction import *
+
+
+def get_test_case(dialect: Dialect) -> str:
+    header = "// RUN: dyn-opt %s | dyn-opt | FileCheck %s"
+    code = dialect.as_str()
+    test = code.split("\n")
+    test = ["// CHECK: " + test_line for test_line in test]
+    test = "\n".join(test)
+    return header + code + test
+
+
+def write_test_case(path: str, dialect: Dialect):
+    f = open(path + dialect.name + ".irdl", "w")
+    f.write(get_test_case(dialect))
+    f.close()
+
+
+def write_all_test_cases(json_path: str, irdl_directory_path: str):
+    f = open(json_path, "r")
+    stats = get_stats_from_json(f.read())
+    for _, dialect in stats.dialects.items():
+        write_test_case(irdl_directory_path, dialect)
+
+
+arg_parser = argparse.ArgumentParser(description='print irdl test cases')
+arg_parser.add_argument("--input-file",
+                        type=str,
+                        nargs="?",
+                        required=True,
+                        help="Path to input file containing all dialects")
+
+arg_parser.add_argument("--test-dir",
+                        type=str,
+                        nargs="?",
+                        required=True,
+                        help="Path to IRDL test directory")
+
+if __name__ == "__main__":
+    args = arg_parser.parse_args()
+    write_all_test_cases(args.input_file, args.test_dir)
